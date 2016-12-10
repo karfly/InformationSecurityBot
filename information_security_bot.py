b8765d88ee98550c8e8b0ca31d6a863298f2d2a8
Karim Iskakov <iskakov@yandex-team.ru>
2016-12-10 23:03:29 +0300

    Added first version of bot

---
+++ information_security_bot.py
@@ -0,0 +1,46 @@
+# import sys
+import telepot
+from telepot.delegate import per_chat_id, create_open, pave_event_space
+
+from person_info_retriever import PersonInfoRetriever
+
+# Logging
+import logging
+
+logging.basicConfig(filename='log', filemode='a',
+                    format='%(asctime)s %(message)s', datefmt='%H:%M:%S',
+                    level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+
+
+class InformationSecurityBot(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(InformationSecurityBot, self).__init__(*args, **kwargs)
+        self.person_info_retriever = PersonInfoRetriever()
+
+        with open('message_templates/start_message.txt') as fin:
+            self.start_message = fin.read()
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type == 'text':
+            text = msg['text']
+
+            if text == '/start start' or text == '/start':
+                self.sender.sendMessage(self.start_message)
+            else:
+                answer = self.person_info_retriever.get_person_info_by_full_name(text)
+                self.sender.sendMessage(answer)
+
+
+if __name__ == '__main__':
+    with open('.information_security_bot_token') as fin:
+        token = fin.read()
+
+    bot = telepot.DelegatorBot(token, [
+        pave_event_space()(
+            per_chat_id(), create_open, InformationSecurityBot, timeout=86400),
+    ])
+
+    bot.message_loop(run_forever='Listening ...')
---
+++ person_info_retriever.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+
+from string import Template
+
+
+class PersonInfoRetriever(object):
+    def __init__(self):
+        with open('message_templates/answer_template.txt') as fin:
+            self.answer_template = Template(fin.read())
+
+    @staticmethod
+    def parse_table_line(table_line):
+        td_tags_texts = list(map(lambda x: x.text, table_line.find_all('td')[2:]))
+        person_info = {
+            'full_name': td_tags_texts[0],
+            'seminarian_surname': td_tags_texts[1],
+            'test_1': td_tags_texts[2],
+            'test_2': td_tags_texts[3],
+            'test_3': td_tags_texts[4],
+            'test_4': td_tags_texts[5],
+            'test_5': td_tags_texts[6],
+            'essay': td_tags_texts[10],
+            'project': td_tags_texts[11],
+            'rating': td_tags_texts[13],
+            'auto_exam_pass': td_tags_texts[15]
+        }
+
+        return person_info
+
+    @staticmethod
+    def find_person_info_by_full_name(full_name):
+        rating_url = 'https://docs.google.com/spreadsheets/u/1/d/19w5v_GkkijkoJnpQTHrok6hWc_ZWaofKzjfPZorKeGU/pubhtml?gid=2100108390&single=true'
+        r = requests.get(rating_url)  # TODO: check return value
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        tr_tags = soup.find_all('tr', {'style': 'height:19px;'})[2:]
+        info = {
+            'n_students': len(tr_tags),
+            'n_students_with_auto_exam_pass': len(tr_tags) // 3
+        }
+
+        for position, table_line in enumerate(tr_tags):
+            info.update(PersonInfoRetriever.parse_table_line(table_line))
+            info['position'] = position + 1
+
+            if full_name in info['full_name']:
+                return info
+
+        return None
+
+    def format_person_info(self, person_info):
+        return self.answer_template.substitute(person_info)
+
+    def get_person_info_by_full_name(self, full_name):
+        person_info = PersonInfoRetriever.find_person_info_by_full_name(full_name)
+        if person_info is not None:
+            return self.format_person_info(person_info)
+        else:
+            return 'Не смог ничего найти :( Возможно вы ошиблись в ФИО?\n'
+
+if __name__ == '__main__':
+    person_info_retriever = PersonInfoRetriever()
+    print(person_info_retriever.get_person_info_by_full_name('Искаков'))
